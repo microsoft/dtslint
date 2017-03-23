f047731f3e798b40d2b0ef63532e61620d221e3f
Andy Hanson <anhans@microsoft.com>
2017-03-23 08:36:02 -0700

    Allow test files to have relative imports of other test files

--- src/rules/noRelativeImportInTestRule.ts
+++ src/rules/noRelativeImportInTestRule.ts
@@ -16,17 +16,30 @@ export class Rule extends Lint.Rules.AbstractRule {
 			return [];
 		}
 
-		return this.applyWithFunction(sourceFile, walk);
+		return this.applyWithFunction(sourceFile, ctx => walk(ctx, global.program));
 	}
 }
 
 const FAILURE_STRING = "Test file should not use a relative import. " +
 	"Use a global import as if this were a user of the package.";
 
-function walk(ctx: Lint.WalkContext<void>): void {
-	for (const i of ctx.sourceFile.imports) {
+function walk(ctx: Lint.WalkContext<void>, program: ts.Program): void {
+	// See https://github.com/palantir/tslint/issues/1969
+	const sourceFile = program.getSourceFile(ctx.sourceFile.fileName);
+	const checker = program.getTypeChecker();
+
+	for (const i of sourceFile.imports) {
 		if (i.text.startsWith(".")) {
-			ctx.addFailureAtNode(i, FAILURE_STRING);
+			const moduleSymbol = checker.getSymbolAtLocation(i);
+			if (!moduleSymbol || !moduleSymbol.declarations) {
+				continue;
+			}
+
+			for (const decl of moduleSymbol.declarations) {
+				if (decl.kind === ts.SyntaxKind.SourceFile && (decl as ts.SourceFile).isDeclarationFile) {
+					ctx.addFailureAtNode(i, FAILURE_STRING);
+				}
+			}
 		}
 	}
 }
