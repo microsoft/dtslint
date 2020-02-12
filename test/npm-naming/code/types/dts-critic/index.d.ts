ac13820ee18bc0f37e21cb7249b75ab30f4607e3
Gabriela Araujo Britto <t-gaar@microsoft.com>
2020-02-12 13:58:23 -0800

    Update npm-naming for new dts-critic

---
+++ test/npm-naming/code/types/dts-critic/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for package dts-critic 1.0
+// Project: https://https://github.com/DefinitelyTyped/dts-critic
+// Definitions by: Jane Doe <https://github.com/janedoe>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export default dtsCritic();
